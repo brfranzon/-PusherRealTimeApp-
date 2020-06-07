9feb3ac2c0215bf9f4109a1811626982549b916f
franzon <brfranzon@gmail.com>
2020-06-07 21:31:31 +0200

    commit

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     // which option was checked?
-       const choice = document.querySelector('input[name=os]:checked').value;
+    const choice = document.querySelector('input[name=os]:checked').value;
 
 
     // data to send
@@ -14,7 +14,7 @@ form.addEventListener("submit", (e) => {
     // resquest to the server
     let url = "https://franzon-voterealtime.herokuapp.com/poll";
 
-   // let url = "http://192.168.1.24:3000/poll"
+    //let url = "http://192.168.1.24:3000/poll"
     fetch(url,
         {
             method: 'post',
@@ -26,88 +26,103 @@ form.addEventListener("submit", (e) => {
         .then(data => console.log(data))
         .catch(err => console.log(err));
 
-    getdata();
 });
 
 
 // read data from server: get request
 //let url = "https://franzon-voterealtime.herokuapp.com/poll";
-getdata();
-function getdata() {
-    //let url = "http://192.168.1.24:3000/poll"
-    let url = "https://franzon-voterealtime.herokuapp.com/poll";
-    fetch(url).
-        then(res => res.json()).
-        then(data => {
-            const votes = data.votes;
-            const totalVotes = votes.length;
 
-            // count vote points
-            const voteCounts = votes.reduce(
-                (acc, vote) => (
-                    (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc),
-                {}
+
+//let url = "http://192.168.1.24:3000/poll"
+let url = "https://franzon-voterealtime.herokuapp.com/poll";
+fetch(url).
+    then(res => res.json()).
+    then(data => {
+        const votes = data.votes;
+        const totalVotes = votes.length;
+
+        // count vote points
+        const voteCounts = votes.reduce(
+            (acc, vote) => (
+                (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc),
+            {}
+        );
+        // canvas js
+        //Initial data points
+        let dataPoints = [
+            { label: "Windows", y: voteCounts.Windows },
+            { label: "MacOS", y: voteCounts.MacOS },
+            { label: "Linux", y: voteCounts.Linux },
+            { label: "Other", y: voteCounts.Other },
+        ];
+
+        if (chart) {
+
+            const chart = new CanvasJS.Chart("chart",
+                {
+                    animationEnabled: true,
+                    theme: "theme1",
+
+                    title: {
+                        //  text: `Anzahl der Wahlen: ${totalVotes}`
+                    },
+
+                    data: [
+                        {
+                            type: "column",
+                            dataPoints: dataPoints
+                        }
+                    ]
+
+                }
             );
 
-            // canvas js
-            //Initial data points
-            let dataPoints = [
-                { label: "Windows", y: voteCounts.Windows },
-                { label: "MacOS", y: voteCounts.MacOS },
-                { label: "Linux", y: voteCounts.Linux },
-                { label: "Other", y: voteCounts.Other },
-            ];
+            chart.render();
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
 
-            if (chart) {
+            var pusher = new Pusher('af7bbbb7eb67604b0148', {
+                cluster: 'eu'
+            });
 
-                const chart = new CanvasJS.Chart("chart",
-                    {
-                        animationEnabled: true,
-                        theme: "theme1",
+            var channel = pusher.subscribe('os-poll');
+            channel.bind('os-vote', function (data) {
 
-                        title: {
-                            text: `Anzahl der Wahlen: ${totalVotes}`
-                        },
+                // add data to the chart
 
-                        data: [
-                            {
-                                type: "column",
-                                dataPoints: dataPoints
-                            }
-                        ]
+                dataPoints = dataPoints.map(elem => {
 
+                    if (elem.label == data.os) {
+                        elem.y = elem.y + data.points;
+
+                        console.log(elem.label, elem.y);
+
+                        return elem;
+                    } else {
+                        return elem;
                     }
-                );
+                });
+
+                let text = document.getElementById("text_total_votes");
+
+                let total = 0;
+                dataPoints.forEach(os => {
+                    total = total + os.y;
+                })
+
+                text.innerHTML = `Anzahl der Stimmen <span style="color:blue">${total}<span>`;
 
                 chart.render();
-                // Enable pusher logging - don't include this in production
-                Pusher.logToConsole = true;
 
-                var pusher = new Pusher('af7bbbb7eb67604b0148', {
-                    cluster: 'eu'
-                });
+            });
 
-                var channel = pusher.subscribe('os-poll');
-                channel.bind('os-vote', function (data) {
-
-                    // add data to the chart
-
-                    dataPoints = dataPoints.map(elem => {
-
-                        if (elem.label == data.os) {
-                            elem.y = elem.y + data.points;
-                            return elem;
-                        } else {
-                            return elem;
-                        }
-                    });
-                    chart.render();
-
-                });
-            }
         }
-        );
-}
+
+    });
+
+
+
+
 
 
 //function togglePopup() {
